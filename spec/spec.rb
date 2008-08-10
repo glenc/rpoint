fb2d687540fd86bf308ca96648aef0df449e978b
Glen Cooper <glen@glenc.net>
2008-08-10 09:14:46 -0700

    Updated spec runner to run either all specs or a given sub-set

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,5 +1,17 @@
-# load all specs
-specs = Dir.glob "*/**/*_spec.rb"
+BASE_DIR = File.dirname(__FILE__)
+
+specs = []
+if ARGV.empty?
+  specs = Dir.glob("#{BASE_DIR}/**/*_spec.rb").uniq
+else
+  ARGV.each do |a|
+    specs << Dir.glob("#{BASE_DIR}/#{a}/**/*_spec.rb").uniq
+  end
+  specs.flatten!.uniq!
+end
+
+puts "Running Specs:"
+puts specs
 
 # run them all
 specs.each do |spec|
